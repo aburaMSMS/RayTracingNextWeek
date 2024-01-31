17cdf8fe6939e77c9cfdc00f23c83c939a68a15a
ZCL <zcl513281304@163.com>
2024-02-01 01:43:04 +0800

    7.4 Cornell Box

--- 7.Lights/main7.cpp
+++ 7.Lights/main7.cpp
@@ -12,31 +12,38 @@ int main()
 {
     HittableList world;
 
-    auto pertext = std::make_shared<NoiseTexture>(4.);
-    world.Add(std::make_shared<Sphere>(Point3{ 0., -1000., 0. }, 1000.,
-        std::make_shared<Lambertian>(pertext)));
-    world.Add(std::make_shared<Sphere>(Point3{ 0., 2., 0. }, 2., std::make_shared<Lambertian>(pertext)));
+    auto red = std::make_shared<Lambertian>(Color(0.65, 0.05, 0.05));
+    auto white = std::make_shared<Lambertian>(Color(0.73, 0.73, 0.73));
+    auto green = std::make_shared<Lambertian>(Color(0.12, 0.45, 0.15));
+    auto light = std::make_shared<DiffuseLight>(Color(15, 15, 15));
 
-    auto diffuse_light = std::make_shared<DiffuseLight>(Color(4.));
-    world.Add(std::make_shared<Sphere>(Point3{ 0., 7., 0. }, 2, diffuse_light));
-    world.Add(std::make_shared<Quadrilateral>(Point3{ 3., 1., -2. }, Vector3{ 2., 0., 0. },
-        Vector3{ 0., 2., 0. }, diffuse_light));
+    world.Add(std::make_shared<Quadrilateral>
+        (Point3{ 555, 0, 0 }, Vector3{ 0, 555, 0 }, Vector3{ 0, 0, 555 }, green));
+    world.Add(std::make_shared<Quadrilateral>
+        (Point3{ 0, 0, 0 }, Vector3{ 0, 555, 0 }, Vector3{ 0, 0, 555 }, red));
+    world.Add(std::make_shared<Quadrilateral>
+        (Point3{ 343, 554, 332 }, Vector3{ -130, 0, 0 }, Vector3{ 0, 0, -105 }, light));
+    world.Add(std::make_shared<Quadrilateral>
+        (Point3{ 0, 0, 0 }, Vector3{ 555, 0, 0 }, Vector3{ 0, 0, 555 }, white));
+    world.Add(std::make_shared<Quadrilateral>
+        (Point3{ 555, 555, 555 }, Vector3{ -555, 0, 0 }, Vector3{ 0, 0, -555 }, white));
+    world.Add(std::make_shared<Quadrilateral>
+        (Point3{ 0, 0, 555 }, Vector3{ 555, 0, 0 }, Vector3{ 0, 555, 0 }, white));
 
     Camera camera;
 
-    camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 800;
-    camera.samples_per_pixel = 200;
+    camera.aspect_ratio = 1.0;
+    camera.image_width = 600;
+    camera.samples_per_pixel = 500;
     camera.max_depth = 50;
     camera.background = Color{ 0. };
 
-    camera.vertical_fov = 20.;
-    camera.look_from = Point3{ 26., 3., 6. };
-    camera.look_at = Point3{ 0. ,2., 0. };
-    camera.view_up = Vector3{ 0., 1, 0. };
+    camera.vertical_fov = 40.;
+    camera.look_from = Point3{ 278, 278, -800 };
+    camera.look_at = Point3{ 278, 278, 0 };
+    camera.view_up = Vector3{ 0, 1, 0 };
 
-    camera.defocus_angle = 0.;
-
+    camera.defocus_angle = 0;
 
     camera.Render(world);
 }
